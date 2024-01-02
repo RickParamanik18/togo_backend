4bead1773ab898c839adf68271a48c2e7932ba1c
Rick Paramanik <rickkcir100@gmail.com>
2024-01-02 14:56:06 +0530

    directory setup done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 8000, () =>
+    console.log(`SERVER RUNNING ON ${process.env.PORT || 8000}`)
+);
+
+app.get("/", (req, res) => {
+    res.send("Server Running fine!!");
+});
