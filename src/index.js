bbffba8957aba60ffba7c45e47454bb00b6fdf33
Rick Paramanik <rickkcir100@gmail.com>
2024-01-02 22:53:13 +0530

    util files added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(process.env.PORT || 8000, () =>
     console.log(`SERVER RUNNING ON ${process.env.PORT || 8000}`)
---
+++ src/utils/apiError.util.js
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+    constructor(statusCode, message, errors = [], stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = null;
+        this.success = false;
+        this.errors = errors;
+    }
+}
+
+export default ApiError;
