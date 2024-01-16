d721e3a25e760cde26a25a11d7330df8b5827733
Rick Paramanik <rickkcir100@gmail.com>
2024-01-16 20:48:47 +0530

    minor change in async handler

--- src/utils/asyncHandler.util.js
+++ src/utils/asyncHandler.util.js
@@ -3,7 +3,7 @@ const asyncHandler = (fn) => {
         try {
             await fn(req, res, next);
         } catch (err) {
-            res.status(err.code || 500).json({
+            res.status(500).json({
                 success: false,
                 message: err.message,
             });
